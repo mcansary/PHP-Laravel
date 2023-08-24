4afd1e07eb721eab12f83832f7feaeba3820c518
taro <taro@example.com>
2023-08-24 08:45:02 +0000

    php/laravel07中でmessages.phpを作成

---
+++ lang/ja/messages.php
@@ -0,0 +1,10 @@
+<!----【PHP/Laravel】07中で作成>
+<?php
+
+return [
+    'login' => 'ログイン',
+    'logout' => 'ログアウト',
+    'email' => 'メールアドレス',
+    'password' => 'パスワード',
+    'remember_me' => 'ログイン情報を記憶する',
+];
